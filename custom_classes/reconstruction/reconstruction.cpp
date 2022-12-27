a78d2b7a698d3cdb60bc13190e531dc975f296ab
Robykmp256 <91569490+Robykmp256@users.noreply.github.com>
2022-12-27 23:13:30 +0100

    Rec

--- custom_classes/reconstruction/reconstruction.cpp
+++ custom_classes/reconstruction/reconstruction.cpp
@@ -1,6 +1,5 @@
 #include "reconstruction.hpp"
-
-
+#include <TRandom3.h>
 #include <vector>
 #include "TClonesArray.h"
 #include <TFile.h>
@@ -17,29 +16,42 @@
   TBranch *b1=tree->GetBranch("Vertex");
   TBranch *b2=tree->GetBranch("Hits");
   TClonesArray *hitsArray = new TClonesArray("Hit",100);
-Vertex vertex;
+  Vertex vertex;
   b1->SetAddress(&vertex);
   b2->SetAddress(&hitsArray);
   for(int ev=0;ev<tree->GetEntries();ev++){
-    tree->GetEvent(ev);
-    int numHits=hitsArray->GetEntries();
-    
+      tree->GetEvent(ev);
+      int numHits=hitsArray->GetEntries();
 
-    for(int j=0; j<numHits;j++){
-    Hit *hitptr=(Hit*)hitsArray->At(j);
-    hitsVec.push_back(*hitptr);
+      for(int j=0; j<numHits;j++){
+      Hit *hitptr=(Hit*)hitsArray->At(j);
+      hitsVec.push_back(*hitptr);
     }
+
    zVertVec.push_back(vertex.getZ());
     
   }
 }
+
 void Reconstruction::runReconstruction()
 {
-  Hit Hitrec;
-  for(int y=0;y<hitsVec.size();y++)
-  {
+    Hit Hitrec;
 
-  }
+    for(int j=0;j<hitsVec.size();j++)
+    {
+        Hitrec = Hit(hitsVec[j].getX(), hitsVec[j].getY(), hitsVec[j].getZ(), hitsVec[j].getHitLayer()+1);
+        Hitrec.smearing();
+        hitsVec[j]=Hitrec;
+    }
+
+    int noi=int(gRandom->Rndm()*1000);
+    for(int i=0;i<noi;i++)
+    {
+        Hitrec=Hit();
+        Hitrec.noise();
+        hitsVec.push_back(Hitrec);
+    }
+
   
   /*TFile hfile("recTree.root","RECREATE");
   TTree* recTree=new TTree(fTreeName2.c_str(),"tree of reconstruction");
