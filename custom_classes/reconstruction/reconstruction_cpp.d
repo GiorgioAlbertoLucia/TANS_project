eaa87e13cacbc323371f7a7806a441c61c748d10
Robykmp256 <91569490+Robykmp256@users.noreply.github.com>
2023-01-01 16:32:36 +0100

    Vertex Rec terminated, a me root si rompe, ma non dà errori

--- custom_classes/reconstruction/reconstruction_cpp.d
+++ custom_classes/reconstruction/reconstruction_cpp.d
@@ -2,6 +2,7 @@
 # DO NOT DELETE
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ./../reconstruction/reconstruction_cpp.so: /mnt/c/Users/Utente/Desktop/UNIVERSITA/Magistrale/anno 1/TANS/TANS_project/TANS_project/custom_classes/hit/./../reconstruction/reconstruction.hpp
 ./../reconstruction/reconstruction_cpp.so: /mnt/c/root/include/TObject.h
 ./../reconstruction/reconstruction_cpp.so: /mnt/c/root/include/Rtypes.h
@@ -85,6 +86,8 @@
 ./../reconstruction/reconstruction_cpp.so: /mnt/c/root/include/RVersion.h /mnt/c/root/include/RConfig.h /mnt/c/root/include/TClass.h /mnt/c/root/include/TDictAttributeMap.h /mnt/c/root/include/TInterpreter.h /mnt/c/root/include/TROOT.h /mnt/c/root/include/TBuffer.h /mnt/c/root/include/TMemberInspector.h /mnt/c/root/include/TError.h /mnt/c/root/include/RtypesImp.h /mnt/c/root/include/TIsAProxy.h /mnt/c/root/include/TFileMergeInfo.h /mnt/c/root/include/TCollectionProxyInfo.h /mnt/c/root/bin/rootcling
 reconstruction_cpp__ROOTBUILDVERSION= 6.24/02
 =======
+=======
+>>>>>>> 7eced655509c77c924bfe205a251e6a614afb51d
 ./custom_classes/reconstruction/reconstruction_cpp.so: /Users/giogi/Documents/TANS_project/TANS_project/./custom_classes/reconstruction/reconstruction.hpp
 ./custom_classes/reconstruction/reconstruction_cpp.so: /usr/local/Cellar/root/6.26.04_1/include/root/TObject.h
 ./custom_classes/reconstruction/reconstruction_cpp.so: /usr/local/Cellar/root/6.26.04_1/include/root/Rtypes.h
@@ -159,4 +162,7 @@ reconstruction_cpp__ROOTBUILDVERSION= 6.24/02
 ./custom_classes/reconstruction/reconstruction_cpp.so: /usr/local/Cellar/root/6.26.04_1/include/root/TError.h
 ./custom_classes/reconstruction/reconstruction_cpp.so: /usr/local/Cellar/root/6.26.04_1/include/root/RVersion.h /usr/local/Cellar/root/6.26.04_1/include/root/RConfig.h /usr/local/Cellar/root/6.26.04_1/include/root/TClass.h /usr/local/Cellar/root/6.26.04_1/include/root/TDictAttributeMap.h /usr/local/Cellar/root/6.26.04_1/include/root/TInterpreter.h /usr/local/Cellar/root/6.26.04_1/include/root/TROOT.h /usr/local/Cellar/root/6.26.04_1/include/root/TBuffer.h /usr/local/Cellar/root/6.26.04_1/include/root/TMemberInspector.h /usr/local/Cellar/root/6.26.04_1/include/root/TError.h /usr/local/Cellar/root/6.26.04_1/include/root/RtypesImp.h /usr/local/Cellar/root/6.26.04_1/include/root/TIsAProxy.h /usr/local/Cellar/root/6.26.04_1/include/root/TFileMergeInfo.h /usr/local/Cellar/root/6.26.04_1/include/root/TCollectionProxyInfo.h /usr/local/bin/rootcling
 reconstruction_cpp__ROOTBUILDVERSION= 6.26/04
+<<<<<<< HEAD
+>>>>>>> 7eced655509c77c924bfe205a251e6a614afb51d
+=======
 >>>>>>> 7eced655509c77c924bfe205a251e6a614afb51d
