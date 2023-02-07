76175a62a14f7f98e60ae8a328d51984112cd598
GiorgioAlbertoLucia <87222843+GiorgioAlbertoLucia@users.noreply.github.com>
2023-02-07 15:15:00 +0100

    Final upload

--- test/testPlotter.cpp
+++
@@ -1,8 +0,0 @@
-#include "../custom_classes/plotter/plotter.hpp"
-#include "../custom_classes/reconstruction/reconstruction.hpp"
-
-void testPlotter()
-{
-    Reconstruction * rec = Reconstruction::getInstance("config/configFile.txt");
-    rec->runReconstruction();
-}
